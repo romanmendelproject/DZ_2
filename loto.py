fb9c5624fb810f5f3f9d90c4713be5cfc7ffc263
romanmendelproject <romanmendel777@gmail.com>
2019-12-27 20:56:18 +0300

    add loto.py

---
+++ loto.py
@@ -0,0 +1,111 @@
+import random
+import sys
+
+
+class Card:
+    def SetCard(self):
+        card_array = []
+        x, y = 0, 9
+        for i in range(9):
+            card_array_item = []
+            for j in range(2):
+                while True:
+                    card_item = random.randint(x, y)
+                    if x == 0:
+                        x += 1
+                    if card_item not in card_array_item:
+                        card_array_item.append(card_item)
+                        break
+                if j == 1:
+                    card_array_item.append(" ")
+            card_array.append(card_array_item)
+            x += 10
+            y += 10
+            if y == 89:
+                y += 1
+        for z in range(3):
+            while True:
+                card_array_item = random.randint(0, 8)
+                if isinstance(card_array[card_array_item][1], int):
+                    card_array[card_array_item][1] = " "
+                    break
+                else:
+                    continue
+        for i in range(9):
+            random.shuffle(card_array[i])
+        self.card_array = card_array
+
+    def GetCard(self, username, user_card):
+        print('{:#^35}'.format(" "+self.name+" "))
+        for j in range(3):
+            if j > 0:
+                print(f"\r")
+            for i in range(9):
+                print(f"{user_card[i][j]:3}", end=" ")
+        print(f"\n{'#'*35}")
+
+    def search(self, user_card, num_keg):
+        for j in range(9):
+            for i in range(3):
+                if user_card[j][i] == num_keg:
+                    user_card[j][i] = "  X"
+                    self.score += 1
+                    print(f"Текущий счет {self.name}: {self.score}")
+                    if self.score == 15:
+                        print("Вы выиграли")
+                        sys.exit(1)
+                    return True
+                else:
+                    continue
+
+
+class Players(Card):
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+
+class Kegs:
+    def __init__(self):
+        self.keg_gen = self.get_keg()
+
+    def get_keg(self):
+        lst = [x for x in range(1, 90)]
+        random.shuffle(lst)
+        for i, y in enumerate(lst):
+            print('\n')
+            print('Текущий бочонок: {} Осталось в мешке: {}\n'.format(y, 90 - (i+1)))
+            yield y
+
+
+if __name__ == '__main__':
+    Keg = Kegs()
+    player = Players('Игрок')
+    comp = Players('Компьютер')
+    player.SetCard()
+    print("\r")
+    comp.SetCard()
+    print(player.card_array)
+    print(comp.card_array)
+    while True:
+        num_keg = next(Keg.keg_gen)
+        player.GetCard(player.name, player.card_array)
+        comp.GetCard(comp.name, comp.card_array)
+
+        input_player = input('Зачеркнуть цифру? (y/n)')
+        if input_player == 'y':
+            if player.search(player.card_array, num_keg):
+                comp.search(comp.card_array, num_keg)
+                continue
+            else:
+                print('Вы проиграли')
+                sys.exit(1)
+        if input_player == 'n':
+            if player.search(player.card_array, num_keg):
+                print('Вы проиграли')
+                sys.exit(1)
+            elif comp.search(comp.card_array, num_keg):
+                continue
+        else:
+            print('Введите y or n')
+            continue
